7d3d2c334c1754b8274ea8223b25e38a9291154f
Taekjun Kim <taekjunkim1223@gmail.com>
2020-09-17 16:28:48 -0700

    Update run_alexnet.py

--- AlexNet/run_alexnet.py
+++ AlexNet/run_alexnet.py
@@ -5,7 +5,7 @@ import torch.nn
 class AlexNet(nn.Module):
     def __init__(self, init_weights=True):
         super().__init__();
-        # feature
+        ### feature
         self.feature = nn.Sequential(OrderedDict([
                            # nn.Conv2d(input_size,output_size,kernel_size,stride,padding);
                            #     padding: zero-padding on both sides;          
@@ -17,22 +17,25 @@ class AlexNet(nn.Module):
                            # nn.MaxPool2d();
                            #     input: 64x55x55;    output: 64x27x27 <== (55-3)/2 + 1
                            ('maxpool0', nn.MaxPool2d(kernel_size=3, stride=2)), 
-                            
-                            
+                           #     input: 64x27x27;    output: 192x27x27 <== (27+2+2-5)/1 + 1
+                           ('conv1', nn.Conv2d(64, 192, kernel_size=5, padding=2),
+                           ('relu1', nn.ReLU()),
+                           #     input: 192x27x27;    output: 192x13x13 <== (27-3)/2 + 1
+                           ('maxpool1', nn.MaxPool2d(kernel_size=3, stride=2)), 
+                           #     input: 192x13x13;    output: 384x13x13 <== (13+1+1-3)/1 + 1
+                           ('conv2', nn.Conv2d(192, 384, kernel_size=3, padding=1),
+                           ('relu2', nn.ReLU()),
+                           #     input: 384x13x13;    output: 256x13x13 <== (13+1+1-3)/1 + 1
+                           ('conv3', nn.Conv2d(384, 256, kernel_size=3, padding=1),
+                           ('relu3', nn.ReLU()),
+                           #     input: 256x13x13;    output: 256x13x13 <== (13+1+1-3)/1 + 1
+                           ('conv4', nn.Conv2d(384, 256, kernel_size=3, padding=1),
+                           ('relu4', nn.ReLU()),
+                           #     input: 256x13x13;    output: 256x6x6 <== (13-3)/2 + 1
+                           ('maxpool4', nn.MaxPool2d(kernel_size=3, stride=2)), 
                            ]));
-        
-
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),        
-        # classifier
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6)); ### I think it may not be needed, if the input size is already 256x6x6                        
+        ### classifier
         
         
         
